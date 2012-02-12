ab5e4d72465f58d386de95ac98248372a0164034
Nils Laumaille <nils@teampass.net>
2012-02-12 21:20:23 +0100

    v2.1.4
    Languade files updated with new strings but not translated

--- includes/language/dutch_kb.php
+++ includes/language/dutch_kb.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['settings']['cpassman_url'])) {
 
 
 $txt['category'] = "Categorie";
-$txt['kb'] = "Knowledge Base";
+$txt['kb'] = "Knownledge Base";
 $txt['kb_anyone_can_modify'] = "Anyone can modify it";
 $txt['kb_form'] = "Manage entries in KB";
 $txt['new_kb'] = "Add a new KB";
