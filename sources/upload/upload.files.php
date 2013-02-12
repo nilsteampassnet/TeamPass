4f20759ce1d2e6f8ad7b48e1445250de8f757a7c
nilsteampassnet <nils@teampass.net>
2013-02-12 22:10:05 +0100

    Add files previously untracked

---
+++ sources/upload/upload.files.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * @file 		upload.files.php
+ * @author		Nils Laumaillé
+ * @version 	2.1.16
+ * @copyright 	(c) 2009-2012 Nils Laumaillé
+ * @licensing 	GNU AFFERO GPL 3.0
+ * @link		http://www.teampass.net
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+session_start();
+if (!isset($_SESSION['CPM']) || $_SESSION['CPM'] != 1) {
+    die('Hacking attempt...');
+}
+
+//check for session
+if (isset($_POST['PHPSESSID'])) {
+    session_id($_POST['PHPSESSID']);
+} elseif (isset($_GET['PHPSESSID'])) {
+    session_id($_GET['PHPSESSID']);
+} else {
+    handleError('No Session was found.');
+}
+
+// HTTP headers for no cache etc
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$targetDir = $_SESSION['settings']['path_to_files_folder'];
+
+$cleanupTargetDir = true; // Remove old files
+$maxFileAge = 5 * 3600; // Temp file age in seconds
+$valid_chars_regex = 'A-Za-z0-9';	//accept only those characters
+
+// 5 minutes execution time
+@set_time_limit(5 * 60);
+
+// Uncomment this one to fake upload time
+// usleep(5000);
+
+// Get parameters
+$chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+$fileName = isset($_REQUEST["name"]) ? $_REQUEST["name"] : '';
+
+// Clean the fileName for security reasons
+$fileName = preg_replace('/[^\w\._]+/', '_', $fileName);
+$fileName = preg_replace('[^'.$valid_chars_regex.']', '', strtolower(basename($fileName)));
+
+// Make sure the fileName is unique but only if chunking is disabled
+if ($chunks < 2 && file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName)) {
+    $ext = strrpos($fileName, '.');
+    $fileNameA = substr($fileName, 0, $ext);
+    $fileNameB = substr($fileName, $ext);
+
+    $count = 1;
+    while (file_exists($targetDir . DIRECTORY_SEPARATOR . $fileNameA . '_' . $count . $fileNameB)) {
+        $count++;
+    }
+
+    $fileName = $fileNameA . '_' . $count . $fileNameB;
+}
+
+$filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+
+// Create target dir
+if (!file_exists($targetDir)) {
+    @mkdir($targetDir);
+}
+
+// Remove old temp files
+if ($cleanupTargetDir && is_dir($targetDir) && ($dir = opendir($targetDir))) {
+    while (($file = readdir($dir)) !== false) {
+        $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+
+        // Remove temp file if it is older than the max age and is not the current file
+        if (preg_match('/\.part$/', $file)
+            && (filemtime($tmpfilePath) < time() - $maxFileAge)
+            && ($tmpfilePath != "{$filePath}.part")
+        ) {
+            @unlink($tmpfilePath);
+        }
+    }
+
+    closedir($dir);
+} else {
+    die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+}
+
+// Look for the content type header
+if (isset($_SERVER["HTTP_CONTENT_TYPE"])) {
+    $contentType = $_SERVER["HTTP_CONTENT_TYPE"];
+}
+
+if (isset($_SERVER["CONTENT_TYPE"])) {
+    $contentType = $_SERVER["CONTENT_TYPE"];
+}
+
+// Handle non multipart uploads older WebKit versions didn't support multipart in HTML5
+if (strpos($contentType, "multipart") !== false) {
+    if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
+        // Open temp file
+        $out = fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+        if ($out) {
+            // Read binary input stream and append it to temp file
+            $in = fopen($_FILES['file']['tmp_name'], "rb");
+
+            if ($in) {
+                while ($buff = fread($in, 4096)) {
+                    fwrite($out, $buff);
+                }
+            } else {
+                die(
+                    '{"jsonrpc" : "2.0",
+                    "error" : {"code": 101, "message": "Failed to open input stream."},
+                    "id" : "id"}'
+                );
+            }
+            fclose($in);
+            fclose($out);
+            @unlink($_FILES['file']['tmp_name']);
+        } else {
+            die(
+                '{"jsonrpc" : "2.0",
+                "error" : {"code": 102, "message": "Failed to open output stream."},
+                "id" : "id"}'
+            );
+        }
+    } else {
+        die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+    }
+} else {
+    // Open temp file
+    $out = fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+    if ($out) {
+        // Read binary input stream and append it to temp file
+        $in = fopen("php://input", "rb");
+
+        if ($in) {
+            while ($buff = fread($in, 4096)) {
+                fwrite($out, $buff);
+            }
+        } else {
+            die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+        }
+
+        fclose($in);
+        fclose($out);
+    } else {
+        die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+    }
+}
+
+// Check if file has been uploaded
+if (!$chunks || $chunk == $chunks - 1) {
+    // Strip the temp .part suffix off
+    rename("{$filePath}.part", $filePath);
+}
+
+if (isset($_POST["type_upload"]) && $_POST["type_upload"] == "import_items_from_csv") {
+    rename($filePath, $targetDir . DIRECTORY_SEPARATOR . $_POST["csvFile"]);
+} else if (isset($_POST["type_upload"]) && $_POST["type_upload"] == "import_items_from_keypass") {
+    rename($filePath, $targetDir . DIRECTORY_SEPARATOR . $_POST["xmlFile"]);
+} else {
+    rename($filePath, $targetDir . DIRECTORY_SEPARATOR . $_POST["File"]);
+}
+
+// Return JSON-RPC response
+die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
+
+/* Handles the error output. */
+function handleError($message)
+{
+    echo $message;
+    exit(0);
+}
