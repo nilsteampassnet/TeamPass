d20e65c4e256566e48ecf9828a31af57563e4564
nils@teampass.net <nils@teampass.net>
2013-12-04 21:47:45 +0100

    2.1.19
    #413 - fix for PHP Parse error: syntax error, unexpected '['
    #447 - fix for PHP Fatal error: Cannot redeclare getBits()
    #442 - problem edit folder
    #399 - Export encrypted passwords (off-line mode)
    #408 - Personal Salt Key changing doesn't work
    #419 - Password complexity not refreshed
    #418 - English translation improvement
    #407 - "Restricted to" feature improvement
    #402 - In item list, description is cut with <br />
    #393 - Password input and confirmation field location
    #388 - Unable to move items between folders
    #400 - Extra fields for Item
    #414 - Maintenance mode during upgrade can be disabled
    #389 - Language dropdown not working
    #392 - Check of absolute path for SK.PHP
    #385 - Email not sent ... check your configuration (to be checked)
    #379 - CSV importing not working (to be checked)
    #134 - Login After Session Expires
    #429 - Changed user.psk field to allow NULLs
    #428 : error: iconv(): Detected an illegal character in input string
    #426/#430 : New option to disable information loading in Admin page
    #142 - Google Authenticator implemented
    * Dialogbox not closed when changing folder name
    * Display Item details through Find page error

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,7 +1,7 @@
 /**
  * @file 		  functions.js
  * @author        Nils Laumaillé
- * @version       2.1.18
+ * @version       2.1.19
  * @copyright     (c) 2009-2013 Nils Laumaillé
  * @licensing     GNU AFFERO GPL 3.0
  * @link    	  http://www.teampass.net
