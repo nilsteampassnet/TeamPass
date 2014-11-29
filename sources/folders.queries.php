4a789ba7b9c3f4e2e8ba484f8be013f256475a6c
Yoann Laissus <yoann.laissus@gmail.com>
2014-11-29 22:24:11 +0100

    Fix rights based on roles for new folders.

--- sources/folders.queries.php
+++ sources/folders.queries.php
@@ -386,14 +386,15 @@ if (isset($_POST['newtitle'])) {
                     }
 
                     //If it is a subfolder, then give access to it for all roles that allows the parent folder
-                    $rows = DB::query("SELECT role_id FROM ".$pre."roles_values WHERE folder_id = %i", $parentId);
+                    $rows = DB::query("SELECT role_id, type FROM ".$pre."roles_values WHERE folder_id = %i", $parentId);
                     foreach ($rows as $record) {
                         //add access to this subfolder
                         DB::insert(
                             $pre.'roles_values',
                             array(
                                 'role_id' => $record['role_id'],
-                                'folder_id' => $newId
+                                'folder_id' => $newId,
+                                'type' => $record['type']
                            )
                         );
                     }
