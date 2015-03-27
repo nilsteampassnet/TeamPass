9aa36a443b1046c3f89a4722f351a58d8bc16354
nilsteampassnet <nils@teampass.net>
2015-03-27 07:55:11 +0100

    Removed yubico non ne cessary files

--- includes/libraries/Authentication/Yubico/example/db.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists demoserver; 
-create table demoserver (
-       id varchar(60) unique not null,
-       username varchar(60) default '' not null,
-       password varchar(60) default '' not null,
-
-       primary key (id)
-);
