6bc6eb93a91e4a8bdfc1f2a84edd8c148fffa104
Nils Laumaillé <nils@teampass.net>
2016-07-19 22:41:54 +0200

    2.1.26
    Fix for #1384

--- install/install.queries.php
+++ install/install.queries.php
@@ -392,7 +392,10 @@ global \$SETTINGS;
                             array('admin','enable_server_password_change','0'),
                             array('admin','ldap_object_class','0'),
                             array('admin','bck_script_path', $var['abspath']."/backups"),
-                            array('admin','bck_script_filename', 'bck_cpassman')
+                            array('admin','bck_script_filename', 'bck_cpassman'),
+                            array('admin','syslog_enable','0'),
+                            array('admin','syslog_host','localhost'),
+                            array('admin','syslog_port','514')
                         );
                         foreach ($aMiscVal as $elem) {
                             //Check if exists before inserting
