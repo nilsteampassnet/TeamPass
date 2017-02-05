0f32d33269d31302c766353be4da9549226d58e3
LAUMAILLE Nils <298792@dom2.ad.sys>
2017-02-05 14:38:02 +0100

    2.1.27
    Missing file

---
+++ install/upgrade_run_defuse_for_logs.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @file          upgrade_run_defuse_for_logs.php
+ * @author        Nils Laumaillé
+ * @version       2.1.27
+ * @copyright     (c) 2009-2016 Nils Laumaillé
+ * @licensing     GNU AFFERO GPL 3.0
+ * @link          http://www.teampass.net
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*
+** Upgrade script for release 2.1.27
+*/
+require_once('../sources/SecureHandler.php');
+session_start();
+error_reporting(E_ERROR | E_PARSE);
+$_SESSION['db_encoding'] = "utf8";
+$_SESSION['CPM'] = 1;
+
+// if already defused
+if (isset($_SESSION['tp_defuse_installed']) && $_SESSION['tp_defuse_installed'] === true) {
+    echo '[{"finish":"1" , "next":"" , "error":""}]';
+    return false;
+}
+
+require_once '../includes/language/english.php';
+require_once '../includes/config/include.php';
+require_once '../includes/config/settings.php';
+require_once '../sources/main.functions.php';
+
+$_SESSION['settings']['loaded'] = "";
+
+$finish = false;
+$next = ($_POST['nb'] + $_POST['start']);
+
+$dbTmp = mysqli_connect(
+    $_SESSION['db_host'],
+    $_SESSION['db_login'],
+    $_SESSION['db_pw'],
+    $_SESSION['db_bdd'],
+    $_SESSION['db_port']
+);
+
+
+// get total items
+$rows = mysqli_query($dbTmp,
+    "SELECT id, pw, pw_iv FROM ".$_SESSION['tbl_prefix']."log_items
+    WHERE encryption_type = 'not_set'"
+);
+if (!$rows) {
+    echo '[{"finish":"1" , "error":"'.mysqli_error($dbTmp).'"}]';
+    exit();
+}
+
+$total = mysqli_num_rows($rows);
+
+// loop on items
+$rows = mysqli_query($dbTmp,
+    "SELECT id_item, raison, raison_iv, encryption_type FROM ".$_SESSION['tbl_prefix']."log_items
+    WHERE encryption_type = 'not_set' LIMIT ".$_POST['start'].", ".$_POST['nb']
+);
+if (!$rows) {
+    echo '[{"finish":"1" , "error":"'.mysqli_error($dbTmp).'"}]';
+    exit();
+}
+
+while ($data = mysqli_fetch_array($rows)) {
+    // extract encrypted string
+    $pwd_tmp = explode(":", $data['raison']);
+
+    if ($data['encryption_type'] !== "defuse" && substr($pwd_tmp[1], 0, 3) !== "def") {
+
+        // decrypt with phpCrypt
+        $old_pw = cryption_phpCrypt(
+            $pwd_tmp[1],
+            $_POST['session_salt'],
+            $data['raison_iv'],
+            "decrypt"
+        );
+
+        // encrypt with Defuse
+        $new_pw = cryption(
+            $old_pw['string'],
+            $_SESSION['new_salt'],
+            "encrypt"
+        );
+
+        // store Password
+        mysqli_query($dbTmp,
+            "UPDATE ".$_SESSION['tbl_prefix']."log_items
+            SET raison = 'at_pw :".$new_pw['string']."', raison_iv = '', encryption_type = 'defuse'
+            WHERE id = ".$data['id']
+        );
+    } else if (substr($pwd_tmp[1], 0, 3) === "def" && $data['encryption_type'] !== "defuse") {
+        mysqli_query($dbTmp,
+            "UPDATE ".$_SESSION['tbl_prefix']."log_items
+            SET encryption_type = 'defuse'
+            WHERE id = ".$data['id']
+        );
+    }
+}
+
+if ($next >= $total) {
+    $finish = 1;
+}
+
+
+echo '[{"finish":"'.$finish.'" , "next":"'.$next.'", "error":""}]';
