81210e456866a1859156e4f2ddcd1d10d5a3c154
LAUMAILLE Nils <298792@dom2.ad.sys>
2017-02-28 18:22:09 +0100

    2.1.27
    Adding security to upgrade process. Admin authentication is required. In progress

--- install/upgrade.php
+++ install/upgrade.php
@@ -108,7 +108,12 @@ if (
         {
             if (step != "") {
                 var upgrade_file = "upgrade_ajax.php";
-                if (step == "step1") {
+                if (step == "step0") {
+                    var data = "type="+step+
+                    "&login="+escape(document.getElementById("user_login").value)+
+                        "&pwd="+aes_encrypt(document.getElementById("user_pwd").value);
+                    document.getElementById("loader").style.display = "";
+                } else if (step == "step1") {
                     var data = "type="+step+
                     "&abspath="+escape(document.getElementById("root_path").value)+
                         "&fullurl="+escape(document.getElementById("root_url").value);
@@ -331,37 +336,51 @@ echo '
                     <input type="hidden" id="actual_cpm_version" name="actual_cpm_version" value="', isset($_POST['actual_cpm_version']) ? $_POST['actual_cpm_version']:'', '" />
                     <input type="hidden" id="cpm_isUTF8" name="cpm_isUTF8" value="', isset($_POST['cpm_isUTF8']) ? $_POST['cpm_isUTF8']:'', '" />
                     <input type="hidden" name="menu_action" id="menu_action" value="" />
+                    <input type="hidden" name="user_granted" id="user_granted" value="" />
                     <input type="hidden" name="session_salt" id="session_salt" value="', (isset($_POST['session_salt']) && !empty($_POST['session_salt'])) ? $_POST['session_salt']:@$_SESSION['encrypt_key'], '" />';
 
 if (!isset($_GET['step']) && !isset($_POST['step'])) {
     //ETAPE O
     echo '
-                     <h2>Teampass upgrade</h2>
+                    <div>
+                    <fieldset>
+                        <legend>Teampass upgrade</legend>
+                        Before starting, be sure to:<ul>
+                        <li>upload the complete package on the server and overwrite existing files,</li>
+                        <li>have the database connection informations,</li>
+                        <li>get some CHMOD rights on the server.</li>
+                        </ul>
 
-                     Before starting, be sure to:<ul>
-                     <li>upload the complete package on the server and overwrite existing files,</li>
-                     <li>have the database connection informations,</li>
-                     <li>get some CHMOD rights on the server.</li>
-                     </ul>
+                        <h5>TeamPass is distributed under GNU AFFERO GPL licence.</h5>
 
-                     <div style="font-weight:bold; color:#C60000; margin-bottom:10px;">
+                        <div style="font-weight:bold; color:#C60000; margin-bottom:10px;">
                         <img src="images/error.png" />&nbsp;ALWAYS BE SURE TO CREATE A DUMP OF YOUR DATABASE BEFORE UPGRADING.
-                     </div>
+                        </div>
+                    </fieldset>
 
-                     <!--
-                     <div style="width:550px; margin-bottom:10px; float:left;">
-                        <label for="dump_done" style="width:350px;">&nbsp;A database dump has been performed</label><input type="checkbox" id="dump_done" name="dump_done" style="">
-                     </div>
-                     -->
+                    <fieldset>
+                        <legend>Authentication</legend>
+                        <div class="ui-state-default ui-corner-all" style="float:left; width:100%; padding:10px; margin:20px 0 20 px;">
+                             <div style="float:left; height:50px; width:100%;">
+                                <label for="user_login" style="width:200px;">Administrator Login:</label>&nbsp;
+                                <input type="text" id="user_login" />
+                             </div>
+                             <div style="float:left;">
+                                <label for="user_pwd" style="width:200px;">Administrator Password:</label>&nbsp;
+                                <input type="password" id="user_pwd" />
+                             </div>
+                        </div>
+                    </fieldset>
 
-                     <br />
-                     <h5>TeamPass is distributed under GNU AFFERO GPL licence.</h5>
-                     &nbsp;
-                     ';
+                    <div style="margin-top:20px;font-weight:bold;text-align:center;height:27px;" id="res_step0"></div>
+                    <input type="hidden" id="step0" name="step0" value="" />
+
+                     </div>';
 
 } elseif (
     (isset($_POST['step']) && $_POST['step'] == 1)
     || (isset($_GET['step']) && $_GET['step'] == 1)
+    && $_SESSION['user_granted'] === "1"
 ) {
     //ETAPE 1
     echo '
@@ -393,6 +412,7 @@ if (!isset($_GET['step']) && !isset($_POST['step'])) {
 } elseif (
     (isset($_POST['step']) && $_POST['step'] == 2)
     || (isset($_GET['step']) && $_GET['step'] == 2)
+    && $_SESSION['user_granted'] === "1"
 ) {
     //ETAPE 2
     echo '
@@ -457,6 +477,7 @@ if (!isset($_GET['step']) && !isset($_POST['step'])) {
 } elseif (
     (isset($_POST['step']) && $_POST['step'] == 3 || isset($_GET['step']) && $_GET['step'] == 3)
     && isset($_POST['actual_cpm_version'])
+    && $_SESSION['user_granted'] === "1"
 ) {
     //ETAPE 3
     echo '
@@ -481,6 +502,7 @@ if (!isset($_GET['step']) && !isset($_POST['step'])) {
 } elseif (
     (isset($_POST['step']) && $_POST['step'] == 4) || (isset($_GET['step'])
     && $_GET['step'] == 4)
+    && $_SESSION['user_granted'] === "1"
 ) {
     //ETAPE 4
 
@@ -506,6 +528,7 @@ if (!isset($_GET['step']) && !isset($_POST['step'])) {
 } elseif (
     (isset($_POST['step']) && $_POST['step'] == 5)
     || (isset($_GET['step']) && $_GET['step'] == 5)
+    && $_SESSION['user_granted'] === "1"
 ) {
     //ETAPE 5
     echo '
@@ -534,6 +557,7 @@ if (!isset($_GET['step']) && !isset($_POST['step'])) {
 } elseif (
     (isset($_POST['step']) && $_POST['step'] == 6)
     || (isset($_GET['step']) && $_GET['step'] == 6)
+    && $_SESSION['user_granted'] === "1"
 ) {
     //ETAPE 5
     echo '
@@ -548,9 +572,10 @@ if (!isset($_GET['step']) && !isset($_POST['step'])) {
 if (!isset($_POST['step'])) {
     echo '
                  <div id="buttons_bottom">
-                     <input type="button" id="but_next" target_id="1" style="padding:3px;cursor:pointer;font-size:20px;" class="ui-state-default ui-corner-all" value="CONTINUE" />
+                     <input type="button" id="but_launch" onclick="Check(\'step0\')" style="padding:3px;cursor:pointer;font-size:20px;" class="ui-state-default ui-corner-all" value="LAUNCH" />
+                    <input type="button" id="but_next" target_id="1" style="padding:3px;cursor:pointer;font-size:20px;" class="ui-state-default ui-corner-all" value="NEXT" disabled="disabled" />
                  </div>';
-} elseif ($_POST['step'] == 3 && $conversion_utf8 == false) {
+} elseif ($_POST['step'] == 3 && $conversion_utf8 == false && $_SESSION['user_granted'] === "1") {
     echo '
                     <div style="width:900px;margin:auto;margin-top:30px;">
                         <div id="progressbar" style="float:left;margin-top:9px;"></div>
@@ -558,7 +583,7 @@ if (!isset($_POST['step'])) {
                             <input type="button" id="but_next" target_id="'. (intval($_POST['step'])+1).'" style="padding:3px;cursor:pointer;font-size:20px;" class="ui-state-default ui-corner-all" value="NEXT" />
                         </div>
                     </div>';
-} elseif ($_POST['step'] == 3 && $conversion_utf8 == true) {
+} elseif ($_POST['step'] == 3 && $conversion_utf8 == true && $_SESSION['user_granted'] === "1") {
     echo '
                     <div style="width:900px;margin:auto;margin-top:30px;">
                         <div id="progressbar" style="float:left;margin-top:9px;"></div>
@@ -567,7 +592,7 @@ if (!isset($_POST['step'])) {
                             <input type="button" id="but_next" target_id="'. (intval($_POST['step'])+1).'" style="padding:3px;cursor:pointer;font-size:20px;" class="ui-state-default ui-corner-all" value="NEXT" disabled="disabled" />
                         </div>
                     </div>';
-} elseif ($_POST['step'] == 6) {
+} elseif ($_POST['step'] == 6 && $_SESSION['user_granted'] === "1") {
     echo '
                  <div style="margin-top:30px; text-align:center; width:100%; font-size:24px;">
                      <a href="#" onclick="javascript:window.location.href=\'', (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') ? 'https' : 'http', '://'.$_SERVER['HTTP_HOST'].substr($_SERVER['PHP_SELF'],0,strrpos($_SERVER['PHP_SELF'],'/')-8).'\';"><b>Open TeamPass</b></a>
