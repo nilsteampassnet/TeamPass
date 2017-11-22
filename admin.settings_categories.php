800724f5cc12045e5fde27ed92c64b64bd5c8347
Anthony Plack <aplack@ncompasssolutions.com>
2017-11-22 16:13:49 -0600

    Changes required to add checkbox category items

--- admin.settings_categories.php
+++ admin.settings_categories.php
@@ -93,7 +93,8 @@ foreach ($rows as $record) {
 
 // Build list of Field Types
 $options_field_types = '<option value="text">'.$LANG['text'].'</option>'.
-                '<option value="masked">'.$LANG['masked'].'</option>';
+                '<option value="masked">'.$LANG['masked'].'</option>'.
+                '<option value="checkbox">'.$LANG['checkbox'].'</option>';
 
 echo '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
