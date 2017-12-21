348c4695163699bc8cb863c49f94296412e93552
nilsteampassnet <nils@teampass.net>
2017-12-21 21:32:20 +0100

    2.1.27
    Adding install folder

---
+++ install/upgrade_run_defuse_for_files_step2.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * @file          upgrade_run_defuse_for_files_step2.php
+ * @author        Nils Laumaillé
+ * @version       2.1.27
+ * @copyright     (c) 2009-2017 Nils Laumaillé
+ * @licensing     GNU AFFERO GPL 3.0
+ * @link          http://www.teampass.net
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*
+** Upgrade script for release 2.1.27
+*/
+require_once('../sources/SecureHandler.php');
+session_start();
+error_reporting(E_ERROR | E_PARSE);
+$_SESSION['db_encoding'] = "utf8";
+$_SESSION['CPM'] = 1;
+
+
+require_once '../includes/language/english.php';
+require_once '../includes/config/include.php';
+require_once '../includes/config/settings.php';
+require_once '../sources/main.functions.php';
+require_once '../includes/config/tp.config.php';
+
+// prepare Encryption class calls
+use \Defuse\Crypto\Crypto;
+use \Defuse\Crypto\File;
+use \Defuse\Crypto\Exception as Ex;
+
+// Prepare POST variables
+$post_nb = filter_input(INPUT_POST, 'nb', FILTER_SANITIZE_NUMBER_INT);
+$post_start = filter_input(INPUT_POST, 'start', FILTER_SANITIZE_NUMBER_INT);
+
+// Some init
+$_SESSION['settings']['loaded'] = "";
+$finish = false;
+$next = ($post_nb + $post_start);
+
+// Test DB connexion
+$pass = defuse_return_decrypted($pass);
+if (mysqli_connect(
+    $server,
+    $user,
+    $pass,
+    $database,
+    $port
+)
+) {
+    $db_link = mysqli_connect(
+        $server,
+        $user,
+        $pass,
+        $database,
+        $port
+    );
+} else {
+    $res = "Impossible to get connected to server. Error is: ".addslashes(mysqli_connect_error());
+    echo '[{"finish":"1", "error":"Impossible to get connected to server. Error is: '.addslashes(mysqli_connect_error()).'!"}]';
+    mysqli_close($db_link);
+    exit();
+}
+
+// if no encryption then stop
+if ($SETTINGS['enable_attachment_encryption'] === "0") {
+    mysqli_query($db_link, "update `".$pre."files` set status = 'clear' where 1 = 1");
+    echo '[{"finish":"1" , "next":"", "error":""}]';
+    exit();
+}
+
+// If $SETTINGS['saltkey_ante_2127'] is set to'none', then encryption is done with Defuse, so exit
+// Also quit if no new defuse saltkey was generated
+if ($SETTINGS['files_with_defuse'] === "done") {
+    echo '[{"finish":"1" , "next":"", "error":""}]';
+    exit();
+}
+
+// get total items
+$rows = mysqli_query($db_link, "SELECT id FROM ".$pre."files");
+if (!$rows) {
+    echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+    exit();
+}
+
+$total = mysqli_num_rows($rows);
+
+// loop on files
+$rows = mysqli_query(
+    $db_link,
+    "SELECT * FROM ".$pre."files
+    LIMIT ".$post_start.", ".$post_nb
+);
+if (!$rows) {
+    echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+    exit();
+}
+
+// load PhpEncryption library
+$path = '../includes/libraries/Encryption/Encryption/';
+
+require_once $path.'Crypto.php';
+require_once $path.'Encoding.php';
+require_once $path.'DerivedKeys.php';
+require_once $path.'Key.php';
+require_once $path.'KeyOrPassword.php';
+require_once $path.'File.php';
+require_once $path.'RuntimeTests.php';
+require_once $path.'KeyProtectedByPassword.php';
+require_once $path.'Core.php';
+
+
+if (file_exists(SECUREPATH."/teampass-seckey.txt")) {
+    // convert KEY
+    $ascii_key = file_get_contents(SECUREPATH."/teampass-seckey.txt");
+    $defuse_key = \Defuse\Crypto\Key::loadFromAsciiSafeString($ascii_key);
+
+    // Prepare decryption options for Defuse
+    $iv = substr(hash("md5", "iv".$ascii_key), 0, 8);
+    $key = substr(hash("md5", "ssapmeat1".$ascii_key, true), 0, 24);
+    $opts_decrypt_defuse = array('iv'=>$iv, 'key'=>$key);
+
+    while ($data = mysqli_fetch_array($rows)) {
+        if (file_exists($SETTINGS['path_to_upload_folder'].'/'.$data['file']) && $data['status'] === 'encrypted') {
+            // Check if file is already encrypted with Defuse.
+            // If yes, then stop
+            try {
+                \Defuse\Crypto\File::decryptFile(
+                    $SETTINGS['path_to_upload_folder'].'/'.$data['file'],
+                    $SETTINGS['path_to_upload_folder'].'/'.$data['file'].".defuse_test",
+                    $defuse_key
+                );
+            } catch (Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException $ex) {
+                // Make a copy of file
+                if (!copy(
+                    $SETTINGS['path_to_upload_folder'].'/'.$data['file'],
+                    $SETTINGS['path_to_upload_folder'].'/'.$data['file'].".copy"
+                )) {
+                    $error = "Copy not possible";
+                    exit;
+                } else {
+                    // do a bck
+                    copy(
+                        $SETTINGS['path_to_upload_folder'].'/'.$data['file'],
+                        $SETTINGS['path_to_upload_folder'].'/'.$data['file'].".bck"
+                    );
+                }
+
+                // Open the file
+                unlink($SETTINGS['path_to_upload_folder'].'/'.$data['file']);
+                $file_primary = fopen($SETTINGS['path_to_upload_folder'].'/'.$data['file'].".copy", "rb");
+                $out = fopen($SETTINGS['path_to_upload_folder'].'/'.$data['file'].".tmp", 'wb');
+
+                // decrypt using old
+                stream_filter_append($file_primary, 'mdecrypt.tripledes', STREAM_FILTER_READ, $opts_decrypt_defuse);
+                // copy to file
+                stream_copy_to_stream($file_primary, $out);
+                // clean
+                fclose($file_primary);
+                fclose($out);
+                unlink($SETTINGS['path_to_upload_folder'].'/'.$data['file'].".copy");
+
+                // Now encrypt the file with new saltkey
+                $err = '';
+                try {
+                    \Defuse\Crypto\File::encryptFile(
+                        $SETTINGS['path_to_upload_folder'].'/'.$data['file'].".tmp",
+                        $SETTINGS['path_to_upload_folder'].'/'.$data['file'],
+                        $defuse_key
+                    );
+                } catch (Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException $ex) {
+                    $err = "encryption_not_possible";
+                } catch (Defuse\Crypto\Exception\EnvironmentIsBrokenException $ex) {
+                    $err = $ex;
+                } catch (Defuse\Crypto\Exception\IOException $ex) {
+                    $err = $ex;
+                }
+                if (empty($err) === false) {
+                    echo $err;
+                }
+
+                // clean
+                unlink($SETTINGS['path_to_upload_folder'].'/'.$data['file'].".tmp");
+            }
+            // Clean if needed
+            if (file_exists($SETTINGS['path_to_upload_folder'].'/'.$data['file'].".defuse_test")) {
+                unlink($SETTINGS['path_to_upload_folder'].'/'.$data['file'].".defuse_test");
+            }
+        }
+    }
+
+    if ($next >= $total) {
+        $finish = 1;
+    }
+} else {
+    echo '[{"finish":"1" , "error":"'.SECUREPATH.'/teampass-seckey.txt does not exist!"}]';
+    exit();
+}
+
+
+echo '[{"finish":"'.$finish.'" , "next":"'.$next.'", "error":""}]';
---
+++ install/upgrade_run_encryption_pwd.php
@@ -0,0 +1,274 @@
+<?php
+/**
+ * @file          upgrade_run_encryption_pwd.php
+ * @author        Nils Laumaillé
+ * @version       2.1.27
+ * @copyright     (c) 2009-2017 Nils Laumaillé
+ * @licensing     GNU AFFERO GPL 3.0
+ * @link          http://www.teampass.net
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+
+require_once('../sources/SecureHandler.php');
+session_start();
+error_reporting(E_ERROR | E_PARSE);
+$_SESSION['db_encoding'] = "utf8";
+$_SESSION['CPM'] = 1;
+
+// if already defused then current instance of Teampss has already been updated to 2.1.27
+if (isset($_SESSION['tp_defuse_installed']) && $_SESSION['tp_defuse_installed'] === true) {
+    echo '[{"finish":"1" , "next":"" , "error":""}]';
+    return false;
+}
+
+
+require_once '../includes/language/english.php';
+require_once '../includes/config/include.php';
+if (!file_exists("../includes/config/settings.php")) {
+    echo 'document.getElementById("res_step1_error").innerHTML = "";';
+    echo 'document.getElementById("res_step1_error").innerHTML = '.
+        '"File settings.php does not exist in folder includes/! '.
+        'If it is an upgrade, it should be there, otherwise select install!";';
+    echo 'document.getElementById("loader").style.display = "none";';
+    exit;
+}
+require_once '../includes/config/settings.php';
+require_once '../sources/main.functions.php';
+
+// Prepare POST variables
+$post_nb = filter_input(INPUT_POST, 'nb', FILTER_SANITIZE_NUMBER_INT);
+$post_start = filter_input(INPUT_POST, 'start', FILTER_SANITIZE_NUMBER_INT);
+
+// Some init
+$next = ($post_nb + $post_start);
+$_SESSION['settings']['loaded'] = "";
+$dbgDuo = fopen("upgrade.log", "a");
+$finish = false;
+
+// Test DB connexion
+$pass = defuse_return_decrypted($pass);
+if (mysqli_connect(
+    $server,
+    $user,
+    $pass,
+    $database,
+    $port
+)
+) {
+    $db_link = mysqli_connect(
+        $server,
+        $user,
+        $pass,
+        $database,
+        $port
+    );
+} else {
+    $res = "Impossible to get connected to server. Error is: ".addslashes(mysqli_connect_error());
+    echo '[{"finish":"1", "error":"Impossible to get connected to server. Error is: '.addslashes(mysqli_connect_error()).'!"}]';
+    mysqli_close($db_link);
+    exit();
+}
+
+fputs($dbgDuo, (string) "\n\nSELECT id, pw, pw_iv FROM ".$pre."items
+    WHERE perso = '0' LIMIT ".$post_start.", ".$post_nb."\n");
+
+// get total items
+$rows = mysqli_query(
+    $db_link,
+    "SELECT id, pw, pw_iv FROM ".$pre."items
+    WHERE perso = '0'"
+);
+if (!$rows) {
+    echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+    exit();
+}
+
+$total = mysqli_num_rows($rows);
+
+// loop on items
+$rows = mysqli_query(
+    $db_link,
+    "SELECT id, pw, pw_iv FROM ".$pre."items
+    WHERE perso = '0' LIMIT ".$post_start.", ".$post_nb
+);
+if (!$rows) {
+    echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+    exit();
+}
+
+while ($data = mysqli_fetch_array($rows)) {
+    fputs($dbgDuo, (string) "\n\n-----\nItem : ".$data['id']);
+
+    // check if pw encrypted with protocol #3
+    if (!empty($data['pw_iv'])) {
+        $pw = cryption_phpCrypt($data['pw'], SALT, $data['pw_iv'], "decrypt");
+        // nothing to do - last encryption protocol (#3) used
+        fputs($dbgDuo, (string) "\nItem is correctly encrypted");
+    } else {
+        if (!empty($data['pw']) && substr($data['pw'], 0, 3) !== "def") {
+            // check if pw encrypted with protocol #2
+            $pw = decrypt($data['pw']);
+            if (empty($pw)) {
+                // used protocol is #1
+                $pw = decryptOld($data['pw']); // decrypt using protocol #1
+            }
+
+            // get key for this pw
+            $resData = mysqli_query(
+                $db_link,
+                "SELECT rand_key FROM ".$pre."keys
+                WHERE `sql_table` = 'items' AND id = ".$data['id']
+            );
+            if (!$resData) {
+                echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+                exit();
+            }
+
+            $dataTemp = mysqli_fetch_row($resData);
+            if (!empty($dataTemp[0])) {
+                // remove key from pw
+                $pw = substr($pw, strlen($dataTemp[0]));
+            }
+
+            // crypt pw with new protocol #3
+            // encrypt pw
+            $encrypt = cryption_phpCrypt($pw, SALT, "", "encrypt");
+
+            // store Password
+            mysqli_query(
+                $db_link,
+                "UPDATE ".$pre."items
+                SET pw = '".$encrypt['string']."',pw_iv = '".$encrypt['iv']."'
+                WHERE id=".$data['id']
+            );
+
+            fputs($dbgDuo, (string) "\nItem has been re-encrypted");
+        } else {
+            // item has no pwd
+            fputs($dbgDuo, (string) "\nItem has no password.");
+        }
+    }
+
+    // does tables KEYS exists
+    if (mysqli_num_rows(mysqli_query("SHOW TABLES LIKE '".$pre."keys'")) == 1) {
+        $table_keys_exists = 1;
+    } else {
+        $table_keys_exists = 0;
+        fputs($dbgDuo, (string) "\nNo re-encryption needed as passwords already using latest encryption protocol.\n");
+    }
+
+    // change log and category fields
+    if ($table_keys_exists == 1) {
+        $resData = mysqli_query(
+            $db_link,
+            "SELECT l.id_item AS id_item, k.rand_key AS rndKey, l.raison AS raison, l.raison_iv AS raison_iv, l.date AS mDate, l.id_user AS id_user, l.action AS action
+            FROM ".$pre."log_items AS l
+            LEFT JOIN ".$pre."keys AS k ON (l.id_item = k.id)
+            WHERE l.id_item = ".$data['id']." AND l.raison LIKE 'at_pw :%' AND k.sql_table='items'"
+        );
+        fputs($dbgDuo, (string) "\nNb of entries in log: ".mysqli_num_rows($resData));
+        while ($record = mysqli_fetch_array($resData)) {
+            fputs($dbgDuo, (string) "\n> ".$record['raison']);
+            if (!empty($record['raison_iv']) && $record['raison_iv'] != null) {
+                // nothing to do
+                fputs($dbgDuo, (string) "Item log correct");
+            } else {
+                // only at_modif and at_pw
+                $reason = explode(' : ', $record['raison']);
+                if (trim($reason[0]) == "at_pw") {
+                    // check if pw encrypted with protocol #2
+                    $pw = decrypt(trim($reason[1]));
+                    fputs($dbgDuo, (string) "\n/ step1 : ".$pw);
+                    if (empty($pw)) {
+                        // used protocol is #1
+                        $pw = decryptOld(trim($reason[1])); // decrypt using protocol #1
+                        fputs($dbgDuo, (string) " / step2 : ".$pw);
+                    }
+
+                    // get key for this pw
+                    $resData_tmp = mysqli_query(
+                        $db_link,
+                        "SELECT rand_key FROM ".$pre."keys
+                        WHERE `sql_table` = 'items' AND id = ".$data['id']
+                    );
+                    if (!$resData_tmp) {
+                        echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+                        exit();
+                    }
+
+                    $dataTemp = mysqli_fetch_row($resData_tmp);
+                    if (!empty($dataTemp[0])) {
+                        // remove key from pw
+                        $pw = substr($pw, strlen($dataTemp[0]));
+                    }
+                    fputs($dbgDuo, (string) " / step3 : ".$pw);
+
+                    // store new encryption
+                    if (isUTF8($pw) && !empty($pw)) {
+                        $encrypt = cryption_phpCrypt($pw, SALT, "", "encrypt");
+                        fputs($dbgDuo, (string) " / Final : ".$encrypt['string']);
+                        mysqli_query(
+                            $db_link,
+                            "UPDATE ".$pre."log_items
+                            SET raison = 'at_pw : ".$encrypt['string']."', raison_iv = '".$encrypt['iv']."'
+                            WHERE id_item =".$data['id']." AND date='".$record['mDate']."'
+                            AND id_user=".$record['id_user']." AND action ='".$record['action']."'"
+                        );
+                    } else {
+                        //data is lost ... unknown encryption
+                    }
+                    fputs($dbgDuo, (string) " / Done.");
+                }
+            }
+        }
+
+        fputs($dbgDuo, (string) "\nLog treatment done.");
+
+        // change category fields encryption
+        $resData = mysqli_query(
+            $db_link,
+            "SELECT i.data AS data, k.rand_key AS rndKey
+            FROM ".$pre."categories_items AS i
+            LEFT JOIN ".$pre."keys AS k ON (k.id = i.item_id)
+            WHERE i.item_id = ".$data['id']." AND k.sql_table='items'"
+        );
+        if (!$resData) {
+            echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+            exit();
+        }
+
+        while ($record = mysqli_fetch_array($resData)) {
+            $tmpData = substr(decrypt($record['data']), strlen($record['rndKey']));
+            if (isUTF8($tmpData) && !empty($tmpData)) {
+                $encrypt = cryption_phpCrypt($tmpData, SALT, "", "encrypt");
+
+                // store Password
+                $resData_tmp2 = mysqli_query(
+                    $db_link,
+                    "UPDATE ".$pre."categories_items
+                        SET data = '".$encrypt['string']."', data_iv = '".$encrypt['iv']."'
+                        WHERE item_id =".$data['id']
+                );
+                if (!$resData_tmp2) {
+                    echo '[{"finish":"1" , "error":"'.mysqli_error($db_link).'"}]';
+                    exit();
+                }
+            } else {
+                //data is lost ... unknown encryption
+            }
+        }
+        fputs($dbgDuo, (string) "\nCategory treatment done.");
+    }
+}
+if ($next >= $total) {
+    $finish = 1;
+}
+
+
+fputs($dbgDuo, (string) "\n\nAll finished.\n");
+
+echo '[{"finish":"'.$finish.'" , "next":"'.$next.'", "error":""}]';
