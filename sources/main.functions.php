6c6af83ad6ecd4a744cd6fe3d533ff9ce41d8ea5
nilsteampassnet <nils@teampass.net>
2019-10-02 17:17:25 +0200

    3.0.0.0

--- sources/main.functions.php
+++ sources/main.functions.php
@@ -3321,7 +3321,7 @@ function filterString($field)
     $field = filter_var(trim($field), FILTER_SANITIZE_STRING);
     if (empty($field) === false) {
         // Load AntiXSS
-        include_once $SETTINGS['cpassman_dir'] . '/includes/libraries/protect/AntiXSS/AntiXSS.php';
+        include_once '../includes/libraries/protect/AntiXSS/AntiXSS.php';
         $antiXss = new voku\helper\AntiXSS();
         // Return
         return $antiXss->xss_clean($field);
