fd1a21c53ab42dfe70bc6c04937639e75e878a9c
BaptisteDaSilva <35465700+BaptisteDaSilva@users.noreply.github.com>
2022-12-14 15:47:10 +0100

    Update otv.php

--- includes/core/otv.php
+++ includes/core/otv.php
@@ -51,168 +51,170 @@ if (file_exists('../includes/config/tp.config.php') === true) {
 require_once $SETTINGS['cpassman_dir'] . '/includes/libraries/protect/SuperGlobal/SuperGlobal.php';
 $superGlobal = new protect\SuperGlobal\SuperGlobal();
 ?>
-<body class="hold-transition login-page ">
-    <div class="login-box" style="margin-top:100px; width:700px;">
-        
+<body class="hold-transition otv-page">
+    <div class="login-box" style="margin-top:100px; width:750px;">
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <a href="../../index.php" class="h1"><b><?php echo TP_TOOL_NAME; ?></b></a>
+                <a href="index.php" class="h1"><b><?php echo TP_TOOL_NAME; ?> - ACTIA Telecom</b></a>
             </div>
             <div class="card-body login-card-body">
-<?php
-if (!empty($superGlobal->get('code', 'GET')) === true
-    && !empty($superGlobal->get('stamp', 'GET')) === true
-    && !empty($superGlobal->get('key', 'GET')) === true
-) {
-    //Include files
-    include_once $SETTINGS['cpassman_dir'].'/includes/config/settings.php';
-    include_once $SETTINGS['cpassman_dir'].'/includes/config/include.php';
-    include_once $SETTINGS['cpassman_dir'].'/sources/SplClassLoader.php';
-    include_once $SETTINGS['cpassman_dir'].'/sources/main.functions.php';
-    // Open MYSQL database connection
-    include_once './includes/libraries/Database/Meekrodb/db.class.php';
-    DB::$host = DB_HOST;
-    DB::$user = DB_USER;
-    DB::$password = defuseReturnDecrypted(DB_PASSWD, $SETTINGS);
-    DB::$dbName = DB_NAME;
-    DB::$port = DB_PORT;
-    DB::$encoding = DB_ENCODING;
-    DB::$ssl = DB_SSL;
-    DB::$connect_options = DB_CONNECT_OPTIONS;
-
-    if (isset($SETTINGS['otv_is_enabled']) === false
-        || (int) $SETTINGS['otv_is_enabled'] === 0
-    ) {
-        echo '
-        <div class="text-center text-danger">
-        <h3><i class="fas fa-exclamation-triangle mr-2"></i>One-Time-View is not allowed!</h3>
-        </div>';
-        exit(true);
-    }
-
-    // check session validity
-    $data = DB::queryfirstrow(
-        'SELECT id, timestamp, code, item_id, encrypted
-        FROM '.prefixTable('otv').'
-        WHERE code = %s',
-        filter_input(INPUT_GET, 'code', FILTER_SANITIZE_STRING)
-    );
-    if (DB::count() > 0  && (int) $data['timestamp'] === (int) filter_input(INPUT_GET, 'stamp', FILTER_VALIDATE_INT)) {
-        // otv is too old
-        if ($data['timestamp'] < time() - ($SETTINGS['otv_expiration_period'] * 86400)) {
-            $html = 'Link is too old!';
-        } else {
-            // get from DB
-            $dataItem = DB::queryfirstrow(
-                'SELECT *
-                FROM '.prefixTable('items').' as i
-                INNER JOIN '.prefixTable('log_items').' as l ON (l.id_item = i.id)
-                WHERE i.id = %i AND l.action = %s',
-                $data['item_id'],
-                'at_creation'
-            );
-            // is Item still valid regarding number of times being seen
-            // Decrement the number before being deleted
-            $dataDelete = DB::queryfirstrow(
-                'SELECT * FROM '.prefixTable('automatic_del').' WHERE item_id=%i',
-                $data['item_id']
-            );
-            if (isset($SETTINGS['enable_delete_after_consultation']) && (int) $SETTINGS['enable_delete_after_consultation'] === 1) {
-                if ((int) $dataDelete['del_enabled'] === 1) {
-                    if ((int) $dataDelete['del_type'] === 1 && (int) $dataDelete['del_value'] >= 1) {
-                        // decrease counter
-                        DB::update(
-                            $pre.'automatic_del',
-                            [
-                                'del_value' => $dataDelete['del_value'] - 1,
-                            ],
-                            'item_id = %i',
-                            $data['item_id']
-                        );
-                    } elseif (((int) $dataDelete['del_type'] === 1 && (int) $dataDelete['del_value'] <= 1)
-                        || ((int) $dataDelete['del_type'] === 2 && (int) $dataDelete['del_value'] < time())
+                <?php
+                if (!empty($superGlobal->get('code', 'GET')) === true
+                    && !empty($superGlobal->get('stamp', 'GET')) === true
+                    && !empty($superGlobal->get('key', 'GET')) === true
+                ) {
+                    //Include files
+                    include_once $SETTINGS['cpassman_dir'].'/includes/config/settings.php';
+                    include_once $SETTINGS['cpassman_dir'].'/includes/config/include.php';
+                    include_once $SETTINGS['cpassman_dir'].'/sources/SplClassLoader.php';
+                    include_once $SETTINGS['cpassman_dir'].'/sources/main.functions.php';
+                    // Open MYSQL database connection
+                    include_once './includes/libraries/Database/Meekrodb/db.class.php';
+                    DB::$host = DB_HOST;
+                    DB::$user = DB_USER;
+                    DB::$password = defuseReturnDecrypted(DB_PASSWD, $SETTINGS);
+                    DB::$dbName = DB_NAME;
+                    DB::$port = DB_PORT;
+                    DB::$encoding = DB_ENCODING;
+                    DB::$ssl = DB_SSL;
+                    DB::$connect_options = DB_CONNECT_OPTIONS;
+                    if (isset($SETTINGS['otv_is_enabled']) === false
+                        || (int) $SETTINGS['otv_is_enabled'] === 0
                     ) {
-                        // delete item
-                        DB::delete($pre.'automatic_del', 'item_id = %i', $data['item_id']);
-                        // make inactive object
-                        DB::update(
-                            prefixTable('items'),
-                            [
-                                'inactif' => '1',
-                            ],
-                            'id = %i',
-                            $data['item_id']
-                        );
-                        // log
-                        logItems(
-                            $SETTIGNS,
-                            (int) $data['item_id'],
-                            $dataItem['label'],
-                            (int) OTV_USER_ID,
-                            'at_delete',
-                            'otv',
-                            'at_automatically_deleted'
-                        );
-                        echo '<div style="padding:10px; margin:90px 30px 30px 30px; text-align:center;" class="ui-widget-content ui-state-error ui-corner-all"><i class="fa fa-warning fa-2x"></i>&nbsp;'.
-                        addslashes($LANG['not_allowed_to_see_pw_is_expired']).'</div>';
-                        return false;
+                        echo '
+                        <div class="text-center text-danger">
+                        <h1><i class="fas fa-exclamation-triangle mr-2"></i>' . langHdl('otv_not_allowed') . '<i class="fas fa-exclamation-triangle mr-2"></i></h3>
+                        </div>';
+                        exit(true);
                     }
-                }
-            }
 
-            // Uncrypt PW
-            $password_decrypted = cryption(
-                $data['encrypted'],
-                filter_input(INPUT_GET, 'key', FILTER_SANITIZE_STRING),
-                'decrypt',
-                $SETTINGS
-            );
-            // get data
-            $label = strip_tags($dataItem['label']);
-            $url = $dataItem['url'];
-            $description = preg_replace('/(?<!\\r)\\n+(?!\\r)/', '', strip_tags($dataItem['description'], TP_ALLOWED_TAGS));
-            $login = str_replace('"', '&quot;', $dataItem['login']);
-            // display data
-            $html = '<div class="text-center">
-                <h3>One-Time item view page</h3>
-                <p class="font-weight-light mt-3">- Here are the details of the Item that has been shared to you -</p>
-                <div class="mt-5">
-                <table class="table text-left" style="margin: 0 auto;">
-                <tr><th>Label:</th><td>'.$label.'</td></tr>
-                <tr><th>Password:</th><td>'.addslashes($password_decrypted['string']).'</td></tr>
-                <tr><th>Description:</th><td>'.$description.'</td></tr>
-                <tr><th>login:</th><td>'.$login.'</td></tr>
-                <tr><th>URL:</th><td>'.$url.'</td></tr>
-                </table></div>
-                <p class="mt-3 text-info"><i class="fas fa-info mr-2"></i>Copy carefully the data you need. This page is only visible once.</div>
-                </div>';
-            // log
-            logItems(
-                $SETTINGS,
-                (int) $data['item_id'],
-                $dataItem['label'],
-                (int) OTV_USER_ID,
-                'at_shown',
-                'otv'
-            );
-            // delete entry
-            DB::delete(prefixTable('otv'), 'id = %i', $data['id']);
-            // display
-            echo $html."</div></div>";
-        }
-    } else {
-        echo '<div class="text-center text-danger">
-        <h3><i class="fas fa-exclamation-triangle mr-2"></i>Not a valid page!</h3>
-        </div>';
-    }
-} else {
-    echo '
-    <div class="text-center text-danger">
-    <h3><i class="fas fa-exclamation-triangle mr-2"></i>No valid OTV parameters!</h3>
-    </div>';
-}
-?>
+                    // check session validity
+                    $data = DB::queryfirstrow(
+                        'SELECT id, timestamp, code, item_id, encrypted
+                        FROM '.prefixTable('otv').'
+                        WHERE code = %s',
+                        filter_input(INPUT_GET, 'code', FILTER_SANITIZE_STRING)
+                    );
+
+                    if (DB::count() > 0  && (int) $data['timestamp'] === (int) filter_input(INPUT_GET, 'stamp', FILTER_VALIDATE_INT)) {
+                        // otv is too old
+                        if ($data['timestamp'] < time() - ($SETTINGS['otv_expiration_period'] * 86400)) {
+                            $html = '<div class="text-center text-danger">
+                            <h1><i class="fas fa-exclamation-triangle mr-2"></i>' . langHdl('otv_link_to_old') . '<i class="fas fa-exclamation-triangle mr-2"></i></h3>
+                            </div>';
+                        } else {
+                            // get from DB
+                            $dataItem = DB::queryfirstrow(
+                                'SELECT *
+                                FROM '.prefixTable('items').' as i
+                                INNER JOIN '.prefixTable('log_items').' as l ON (l.id_item = i.id)
+                                WHERE i.id = %i AND l.action = %s',
+                                $data['item_id'],
+                                'at_creation'
+                            );
+                            // is Item still valid regarding number of times being seen
+                            // Decrement the number before being deleted
+                            $dataDelete = DB::queryfirstrow(
+                                'SELECT * FROM '.prefixTable('automatic_del').' WHERE item_id=%i',
+                                $data['item_id']
+                            );
+                            if (isset($SETTINGS['enable_delete_after_consultation']) && (int) $SETTINGS['enable_delete_after_consultation'] === 1) {
+                                if (!empty($dataDelete) && (int) $dataDelete['del_enabled'] === 1) {
+                                    if ((int) $dataDelete['del_type'] === 1 && (int) $dataDelete['del_value'] >= 1) {
+                                        // decrease counter
+                                        DB::update(
+                                            prefixTable('automatic_del'),
+                                            [
+                                                'del_value' => $dataDelete['del_value'] - 1,
+                                            ],
+                                            'item_id = %i',
+                                            $data['item_id']
+                                        );
+                                    } elseif (((int) $dataDelete['del_type'] === 1 && (int) $dataDelete['del_value'] <= 1)
+                                        || ((int) $dataDelete['del_type'] === 2 && (int) $dataDelete['del_value'] < time())
+                                    ) {
+                                        // delete item
+                                        DB::delete(prefixTable('automatic_del'), 'item_id = %i', $data['item_id']);
+                                        // make inactive object
+                                        DB::update(
+                                            prefixTable('items'),
+                                            [
+                                                'inactif' => '1',
+                                            ],
+                                            'id = %i',
+                                            $data['item_id']
+                                        );
+                                        // log
+                                        logItems(
+                                            $SETTIGNS,
+                                            (int) $data['item_id'],
+                                            $dataItem['label'],
+                                            (int) OTV_USER_ID,
+                                            'at_delete',
+                                            'otv',
+                                            'at_automatically_deleted'
+                                        );
+                                        echo '<div style="padding:10px; margin:90px 30px 30px 30px; text-align:center;" class="ui-widget-content ui-state-error ui-corner-all"><i class="fa fa-warning fa-2x"></i>&nbsp;'.
+                                        addslashes($LANG['not_allowed_to_see_pw_is_expired']).'</div>';
+                                        return false;
+                                    }
+                                }
+                            }
+
+                            // Uncrypt PW
+                            $password_decrypted = cryption(
+                                $data['encrypted'],
+                                filter_input(INPUT_GET, 'key', FILTER_SANITIZE_STRING),
+                                'decrypt',
+                                $SETTINGS
+                            );
+                            // get data
+                            $url = $dataItem['url'];
+                            $description = preg_replace('/(?<!\\r)\\n+(?!\\r)/', '', strip_tags($dataItem['description'], TP_ALLOWED_TAGS));
+                            $login = str_replace('"', '&quot;', $dataItem['login']);
+                            // display data
+                            $html = '
+                            <div class="text-center">
+                                <h3>' . langHdl('otv_view_page_details_share') . '</h3>
+                                <div class="mt-5">
+                                    <table class="table text-left" style="margin: 0 auto; border-collapse: unset; table-layout: unset;">';
+
+                            if (!empty($login)) { $html .= '<tr><th style="width: 150px;">' . langHdl('otv_login') . '</th><td>'.$login.'</td></tr>'; }
+                            if (!empty($password_decrypted['string'])) { $html .= '<tr><th style="width: 150px;">' . langHdl('otv_password') . '</th><td>'.addslashes($password_decrypted['string']).'</td></tr>'; }
+                            if (!empty($description)) { $html .= '<tr><th style="width: 150px;">' . langHdl('otv_description') . '</th><td>'.$description.'</td></tr>'; }
+                            if (!empty($url)) { $html .= '<tr><th style="width: 150px;">' . langHdl('otv_url') . '</th><td><a href="'.$url.'" target="_blank">'.$url.'</a></td></tr>'; }
+
+                            $html .= '
+                                    </table>
+                                </div>
+                                <h3 class="mt-3 text-info">' . langHdl('otv_view_page_information') . '</h3>
+                            </div>';
+                            // log
+                            logItems(
+                                $SETTINGS,
+                                (int) $data['item_id'],
+                                $dataItem['label'],
+                                (int) OTV_USER_ID,
+                                'at_shown',
+                                'otv'
+                            );
+                            // delete entry
+                            DB::delete(prefixTable('otv'), 'id = %i', $data['id']);
+                            // display
+                            echo $html;
+                        }
+                    } else {
+                        echo '<div class="text-center text-danger">
+                        <h1><i class="fas fa-exclamation-triangle mr-2"></i>' . langHdl('otv_not_valid_page') . ' <i class="fas fa-exclamation-triangle mr-2"></i></h3>
+                        </div>';
+                    }
+                } else {
+                    echo '<div class="text-center text-danger">
+                    <h1><i class="fas fa-exclamation-triangle mr-2"></i>' . langHdl('otv_no_valid_parameters') . ' <i class="fas fa-exclamation-triangle mr-2"></i></h3>
+                    </div>';
+                }
+                ?>
             </div>
         </div>
     </div>
