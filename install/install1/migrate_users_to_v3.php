7002ec8292a56f5215bfb43857561141da46763f
nilsteampassnet <nils@teampass.net>
2023-03-24 13:38:15 +0100

    3.0.1
    Merged branch 3.0.1

---
+++ install/install1/migrate_users_to_v3.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Teampass - a collaborative passwords manager.
+ * ---
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * ---
+ * @project   Teampass
+ * @version   3.0.1
+ * @file      migrate_users_to_v3.php
+ * ---
+ * @author    Nils Laumaillé (nils@teampass.net)
+ * @copyright 2009-2023 Teampass.net
+ * @license   https://spdx.org/licenses/GPL-3.0-only.html#licenseText GPL-3.0
+ * ---
+ * @see       https://www.teampass.net
+ */
+
+set_time_limit(600);
+
+
+require_once '../sources/SecureHandler.php';
+session_name('teampass_session');
+session_start();
+error_reporting(E_ERROR | E_PARSE);
+$_SESSION['db_encoding'] = 'utf8';
+$_SESSION['CPM'] = 1;
+
+// Prepare POST variables
+$post_file_number = filter_input(INPUT_POST, 'file_number', FILTER_SANITIZE_NUMBER_INT);
+
+$scripts_list = array(
+    array('upgrade_run_3.0.0_users.php', 'user_id'),
+);
+$param = '';
+
+// test if finished
+if (intval($post_file_number) >= count($scripts_list)) {
+    $finished = 1;
+} else {
+    $finished = 0;
+}
+echo '[{"finish":"' . $finished . '", "scriptname":"' . $scripts_list[$post_file_number][0] . '", "parameter":"' . $scripts_list[$post_file_number][1] . '"}]';
