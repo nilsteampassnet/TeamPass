e214224f2ad26e7928a1d8496a1cde45d2de5026
nilsteampassnet <nils@teampass.net>
2024-01-12 20:58:39 +0100

    3.1.1
    Adding new licences.md file

--- includes/config/include.php
+++ includes/config/include.php
@@ -28,7 +28,7 @@
 
 define('TP_VERSION', '3.1.1');
 define("UPGRADE_MIN_DATE", "1702452416");
-define('TP_VERSION_MINOR', '31');
+define('TP_VERSION_MINOR', '36');
 define('TP_TOOL_NAME', 'Teampass');
 define('TP_ONE_DAY_SECONDS', 86400);
 define('TP_ONE_WEEK_SECONDS', 604800);
