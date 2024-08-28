b0f406eeb19861be8f822cd66c756adfba9fe45d
nilsteampassnet <nils@teampass.net>
2024-08-28 17:55:42 +0200

    3.1.2
    Fixing Fossa licences issues

--- includes/config/include.php
+++ includes/config/include.php
@@ -28,7 +28,7 @@
 
 define('TP_VERSION', '3.1.2');
 define("UPGRADE_MIN_DATE", "1724605220");
-define('TP_VERSION_MINOR', '54');
+define('TP_VERSION_MINOR', '55');
 define('TP_TOOL_NAME', 'Teampass');
 define('TP_ONE_DAY_SECONDS', 86400);
 define('TP_ONE_WEEK_SECONDS', 604800);
