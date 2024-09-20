021d11af7b5b5291595fe8e36376c5e200c1f96c
nilsteampassnet <nils@teampass.net>
2024-09-20 19:56:50 +0200

    3.1.2
    Merged PRs: #4345, #4348, #4347, #4343, #4342, #4341, #4340, #4339, #4338, #4337, #4336, #4335

--- pages/roles.js.php
+++ pages/roles.js.php
@@ -257,7 +257,7 @@ if ($checkUserAccess->checkSession() === false || $checkUserAccess->userAccessPa
                     $('.infotip').tooltip();
 
                     // Adapt select
-                    $('#folders-depth').val('').change();
+                    $('#folders-depth').empty().change();
                     $('#folders-depth').append('<option value="all"><?php echo $lang->get('all'); ?></option>');
                     for (x = 1; x < max_folder_depth; x++) {
                         $('#folders-depth').append('<option value="' + x + '">' + x + '</option>');
