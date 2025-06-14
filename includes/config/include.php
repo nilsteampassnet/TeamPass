855c0135738bf76519998c0fad4eadbfe3633d1b
Nils Laumaillé <nils@teampass.net>
2025-06-14 15:30:06 +0200

    Preparing 3.1.4.24

--- includes/config/include.php
+++ includes/config/include.php
@@ -28,7 +28,7 @@
 
 define('TP_VERSION', '3.1.4');
 define("UPGRADE_MIN_DATE", "1749880695");
-define('TP_VERSION_MINOR', '23');
+define('TP_VERSION_MINOR', '24');
 define('TP_TOOL_NAME', 'Teampass');
 define('TP_ONE_DAY_SECONDS', 86400);
 define('TP_ONE_WEEK_SECONDS', 604800);
