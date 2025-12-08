caf4735e13899f1f71e878ae0a422a2a1cac45a3
Nils Laumaillé <nils@teampass.net>
2025-12-08 18:17:48 +0100

    Fix #4756:Several small issues blocking install and upgrade

--- install/upgrade_scripts_manager.php
+++ install/upgrade_scripts_manager.php
@@ -59,7 +59,6 @@ $scripts_list = array(
     array('upgrade_operations.php', '20231017_1'),
     array('upgrade_run_3.1.php', 'user_id'),
     array('upgrade_operations.php', 'clean_duplicate_sharekeys'),
-    array('upgrade_operations.php', 'clean_duplicate_sharekeys'),
     array('upgrade_run_3.1.5.php', 'user_id'),
     array('upgrade_operations.php', 'Transparent_recovery_migration'),
 );
