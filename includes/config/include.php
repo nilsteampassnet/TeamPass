e418cf0220e65ceb1b1b7ea2390410cd7fa9c9a3
Nils Laumaillé <nils@teampass.net>
2026-01-11 11:20:57 +0100

    chore: update files refrence

--- includes/config/include.php
+++ includes/config/include.php
@@ -27,8 +27,8 @@
  */
 
 define('TP_VERSION', '3.1.5');
-define("UPGRADE_MIN_DATE", "1767812991");
-define('TP_VERSION_MINOR', '23');
+define("UPGRADE_MIN_DATE", "1768126101");
+define('TP_VERSION_MINOR', '24');
 define('TP_TOOL_NAME', 'Teampass');
 define('TP_ONE_DAY_SECONDS', 86400);
 define('TP_ONE_WEEK_SECONDS', 604800);
