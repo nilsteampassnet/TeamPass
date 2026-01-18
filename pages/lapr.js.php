b50103d5cc16d78139768e815934f82771370f6b
Guerric <guerric@agitys.com>
2026-01-18 11:01:42 +0100

    Rename file from laps.js.php to lapr.js.php

--- pages/lapr.js.php
+++ pages/lapr.js.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
  * Certain components of this file may be under different licenses. For
  * details, see the `licenses` directory or individual file headers.
  * ---
- * @file      laps.js.php
+ * @file      lapr.js.php
  * @author    Nils Laumaillé (nils@teampass.net)
  * @copyright 2009-2026 Teampass.net
  * @license   GPL-3.0
@@ -69,7 +69,7 @@ $checkUserAccess = new PerformChecks(
 );
 // Handle the case
 echo $checkUserAccess->caseHandler();
-if ($checkUserAccess->checkSession() === false || $checkUserAccess->userAccessPage('export') === false) {
+if ($checkUserAccess->checkSession() === false || $checkUserAccess->userAccessPage('lapr') === false) {
     // Not allowed page
     $session->set('system-error_code', ERR_NOT_ALLOWED);
     include $SETTINGS['cpassman_dir'] . '/error.php';
