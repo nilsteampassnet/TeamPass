4f299c2681b7110c985f86e3ac36e2d5bbf53109
Guerric <guerric@agitys.com>
2026-01-23 09:28:14 +0100

    Update utilities.logs.php

--- pages/utilities.logs.php
+++ pages/utilities.logs.php
@@ -133,6 +133,7 @@ header('Cache-Control: no-cache, no-store, must-revalidate');
                                         <tr>
                                             <th><?php echo $lang->get('date'); ?></th>
                                             <th><?php echo $lang->get('action'); ?></th>
+                                            <th>Source</th>
                                             <th><?php echo $lang->get('user'); ?></th>
                                         </tr>
                                     </thead>
@@ -182,6 +183,7 @@ header('Cache-Control: no-cache, no-store, must-revalidate');
                                             <th><?php echo $lang->get('folder'); ?></th>
                                             <th><?php echo $lang->get('user'); ?></th>
                                             <th><?php echo $lang->get('action'); ?></th>
+                                            <th>API</th>
                                             <th><?php echo $lang->get('at_personnel'); ?></th>
                                         </tr>
                                     </thead>
