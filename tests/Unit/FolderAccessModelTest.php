fda11888876e285b0f8dab1e7b677ef98e16ba58
Guerric <guerric@agitys.com>
2026-06-09 21:02:19 +0200

    Add tests for FolderAccessModel SQL constraints

--- tests/Unit/FolderAccessModelTest.php
+++ tests/Unit/FolderAccessModelTest.php
@@ -1,5 +1,33 @@
 <?php
+
 declare(strict_types=1);
+
+/**
+ * Teampass - a collaborative passwords manager.
+ * ---
+ * This file is part of the TeamPass project.
+ * 
+ * TeamPass is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * TeamPass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Certain components of this file may be under different licenses. For
+ * details, see the `licenses` directory or individual file headers.
+ * ---
+ * @file      FolderAccessModelTest.php
+ * @author    Teampass Community
+ * @copyright 2009-2026 Teampass.net
+ * @license   GPL-3.0
+ * @see       https://www.teampass.net
+ */
 
 use PHPUnit\Framework\TestCase;
 
@@ -33,5 +61,22 @@ class FolderAccessModelTest extends TestCase
         $model = new FolderAccessModel();
 
         self::assertSame(' AND 1 = 0', $model->getItemFolderSqlConstraint('i.id_tree;DROP', 7));
+    }
+
+    public function testPersonalFolderSqlConstraintOnlyExcludesOtherUsersRoots(): void
+    {
+        $model = new FolderAccessModel();
+
+        $constraint = $model->getItemFolderSqlConstraint('i.id_tree', 7);
+
+        self::assertStringContainsString('other_personal.parent_id = 0', $constraint);
+    }
+
+    public function testPersonalFolderFiltersOnlyExcludeOtherUsersRoots(): void
+    {
+        $source = file_get_contents(__DIR__ . '/../../app/api/Model/FolderAccessModel.php');
+        self::assertIsString($source);
+
+        self::assertSame(3, substr_count($source, 'other_personal.parent_id = 0'));
     }
 }
