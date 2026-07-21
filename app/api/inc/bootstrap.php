a8485413696cb2007c99189b2d7b320d06c7a39d
Dennis <danfossi@users.noreply.github.com>
2026-07-21 12:15:13 +0200

    Update bootstrap.php
    Added readFolders action to user rights controls.

--- app/api/inc/bootstrap.php
+++ app/api/inc/bootstrap.php
@@ -102,7 +102,7 @@ function checkUSerCRUDRights($userData, $actionToPerform): bool
 {
     if ($actionToPerform === 'create' && $userData['allowed_to_create'] === 1) {
         return true;
-    } elseif (in_array($actionToPerform, ['read', 'get', 'inFolders', 'findByUrl', 'getOtp', 'allTags', 'listFolders', 'writableFolders']) === true && $userData['allowed_to_read'] === 1) {
+    } elseif (in_array($actionToPerform, ['read', 'get', 'inFolders', 'findByUrl', 'getOtp', 'allTags', 'listFolders', 'writableFolders', 'readFolders']) === true && $userData['allowed_to_read'] === 1) {
         return true;
     } elseif ($actionToPerform === 'update' && $userData['allowed_to_update'] === 1) {
         return true;
